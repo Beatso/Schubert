ea4299c2f379c9e617e3a57aec69d47c3917bba7
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-11-14 22:37:38 +0000

    basic discord bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+require("dotenv").config
+
+const client = new Discord.Client()
+
+client.once ("ready", _ => {
+	console.log("bot running"),
+	client.user.setActivity("beatso.tk/project/schubert", {type:"WATCHING"})
+})
+
+client.login(process.env.bottoken)
